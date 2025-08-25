03e390128729c4cc45e69acafed2f00a7d10ec01
hridyar007 <hridyar007@gmail.com>
2025-08-25 22:41:19 +0530

    Update battery.cpp

--- battery.cpp
+++ battery.cpp
@@ -4,25 +4,22 @@
 
 using namespace std;
 
+// complexity = 1 (ternary only)
 bool reportStatus(float value, const Range& range) {
     Status s = classify(value, range);
-    if (s != Status::OK) {
-        cout << range.name << " " << statusToString(s) 
-             << " (value=" << value 
-             << ", range=" << range.min << "-" << range.max << ")\n";
-        return false;
-    }
-    return true;
+    cout << range.name << " = " << value 
+         << " -> " << statusToString(s)
+         << " (range " << range.min << "-" << range.max << ")\n";
+    return s == Status::OK;
 }
 
+// complexity = 1 (no branching, only logical &&)
 bool batteryIsOk(float temperature, float soc, float chargeRate) {
     Range temperatureRange = {0, 45, "Temperature"};
     Range socRange         = {20, 80, "State of Charge"};
     Range chargeRateRange  = {0, 0.8, "Charge Rate"};
 
-    bool tempOk  = reportStatus(temperature, temperatureRange);
-    bool socOk   = reportStatus(soc, socRange);
-    bool rateOk  = reportStatus(chargeRate, chargeRateRange);
-
-    return tempOk && socOk && rateOk;
+    return reportStatus(temperature, temperatureRange)
+        && reportStatus(soc, socRange)
+        && reportStatus(chargeRate, chargeRateRange);
 }
